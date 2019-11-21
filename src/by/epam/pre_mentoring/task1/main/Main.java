d85281960c49834432f90d853147a2e9f36c2797
Mikhail_Piatrenka <kyrortnik97@gmail.com>
2019-11-21 17:44:50 +0300

    test workIntegration

--- src/by/epam/pre_mentoring/task1/main/Main.java
+++ src/by/epam/pre_mentoring/task1/main/Main.java
@@ -15,6 +15,7 @@ package by.epam.pre_mentoring.task1.main;
  */
 import by.epam.pre_mentoring.task1.bean.AirCompany;
 import by.epam.pre_mentoring.task1.bean.Airplane;
+import by.epam.pre_mentoring.task1.util.console.ReadConsole;
 
 
 import java.util.ArrayList;
@@ -29,18 +30,21 @@ public class Main {
         AirCompany company = new AirCompany(list);
         String input = " ";
         Scanner sc = new Scanner(System.in);
+        boolean check;
+        ReadConsole console = new ReadConsole();
+        int number;
 
-        list.add(new Airplane(750,10000,"China Inc.","China","white",300000,730,300,500,20000));
-        list.add(new Airplane(950,20000,"AirBUS Inc.","Netherlands","orange",400000,550,600,1000,20000));
-        list.add(new Airplane(1000,15000,"Plane LLC.","Belarus","green",300000,440,250,700,12000));
-        list.add(new Airplane(1000,15000,"Boeing","USA","red",300000,737,450,700,15000));
+        list.add(new Airplane(750, 10000, "China Inc.", "China", "white", 300000, 730, 300, 500, 20000));
+        list.add(new Airplane(950, 20000, "AirBUS Inc.", "Netherlands", "orange", 400000, 550, 600, 1000, 20000));
+        list.add(new Airplane(1000, 15000, "Plane LLC.", "Belarus", "green", 300000, 440, 250, 700, 12000));
+        list.add(new Airplane(1000, 15000, "Boeing", "USA", "red", 300000, 737, 450, 700, 15000));
 
 
         System.out.println("Here all planes of the company (sorted by flight distance:)");
 
         Collections.sort(company.getCompany());
 
-        for (Airplane pl : company.getCompany()){
+        for (Airplane pl : company.getCompany()) {
             System.out.println(pl.toString());
         }
 
@@ -48,24 +52,45 @@ public class Main {
         System.out.println("Gross Seats number on all planes is: " + grossSeat);
 
         int grossTon = company.grossTonnage(company.getCompany());
-        System.out.println("Gross tonnage of all planes is: : "+ grossTon);
+        System.out.println("Gross tonnage of all planes is: : " + grossTon);
 
-        System.out.println("Do you want to find a particular plane? \"Type \"Yes\" to continue, \"No\" to Exit from application");
 
-        do {
-            while (!sc.hasNext()){
-                input = sc.nextLine();
-                break;
+
+
+            System.out.println("Do you want to find a particular plane? \"Type \"Yes\" to continue, \"No\" to Exit from application:");
+
+
+        while (console.readCheck()) {
+
+            System.out.println("Enter model:");
+            number = console.readInt();
+            for (Airplane pl : company.getCompany()) {
+                if (pl.getPlaneModel() == number) {
+                    System.out.println(pl.toString());
+                    break;
+                } else {
+                   continue;
+
+                }
             }
-            if (input == "Yes"){
 
-            }else if (input == "No"){
-
-            }else{
-                System.out.println();
-            }
-        }while (1==1);
+            System.out.println("Do you want to find a particular plane? \"Type \"Yes\" to continue, \"No\" to Exit from application");
+            //some change
 
 
-    }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
 }
---
+++ src/by/epam/pre_mentoring/task1/util/console/ReadConsole.java
@@ -0,0 +1,31 @@
+package by.epam.pre_mentoring.task1.util.console;
+
+import java.util.Scanner;
+
+public class ReadConsole {
+
+    public boolean readCheck(){
+        boolean check;
+        Scanner sc = new Scanner(System.in);
+
+            if (sc.next().equals("Yes")){
+                check = true;
+            }else{
+                check = false;
+            }
+        return  check;
+    }
+
+    public String readString(){
+        String str;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        return str;
+    }
+    public int readInt(){
+        int ch;
+        Scanner sc = new Scanner(System.in);
+        ch = sc.nextInt();
+        return ch;
+    }
+}
